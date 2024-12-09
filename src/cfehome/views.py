8e71dd9ba5e69f694aad9e93723424a7fcc494c3
Kourosh <kourosh@Kourosh.Colossi>
2024-12-09 21:00:37 +0330

    initial commit

---
+++ src/cfehome/views.py
@@ -0,0 +1,22 @@
+import pathlib
+from django.shortcuts import render
+from django.http import HttpResponse
+
+from visits.models import PageVisit
+
+this_dir = pathlib.Path(__file__).resolve().parent
+
+def home_page_view(request, *args, **kwargs):
+    queryset = PageVisit.objects.all()
+
+    my_title = "my_page"
+    my_context = {
+        "page_title": my_title, 
+        "queryset": queryset
+    }
+
+    html_template = "home.html"
+
+    PageVisit.objects.create(path=request.path)
+
+    return render(request, html_template, my_context)
