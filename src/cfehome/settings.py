9f9f2869df464dfc26192692136ea59b8c728c0c
Kourosh <kourosh@Kourosh.Colossi>
2024-12-10 20:26:52 +0330

    create a local cdn

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -143,6 +143,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_BASE_DIR = BASE_DIR / "staticfiles"
+STATICFILES_VENDOR_DIR = STATICFILES_BASE_DIR / "vendors"
+
+STATICFILES_DIR = [
+    STATICFILES_BASE_DIR
+]
+
+STATIC_ROOT = BASE_DIR.parent / "local-cdn"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
